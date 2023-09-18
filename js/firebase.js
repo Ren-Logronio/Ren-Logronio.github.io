6d4d42d34e647dcf5e1863128fd95e0bceee539b
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2023-09-18 23:53:25 +0800

    added integration - email form maker

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+
+    // Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDChdMVn-VNOeqZ5srQ4cw_HXTpmcuZCTg",
+    authDomain: "cv-mail-f7efa.firebaseapp.com",
+    projectId: "cv-mail-f7efa",
+    storageBucket: "cv-mail-f7efa.appspot.com",
+    messagingSenderId: "157157124282",
+    appId: "1:157157124282:web:cb2bcace2e2ed634c0ba83"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
